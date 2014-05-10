9b421efbeae61031714db65155bdcb44d98fa149
Steve Kondik <shade@chemlab.org>
2014-05-10 05:12:46 -0700

    msm8974: Enable QC softap extensions
    Change-Id: Ic8c371f19c872f38a50ec6eea968fb963a920eed

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -139,6 +139,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/oppo/find7a
 
 # Wifi
 BOARD_HAS_QCOM_WLAN              := true
+BOARD_HAS_QCOM_WLAN_SDK          := true
 BOARD_WLAN_DEVICE                := qcwcn
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
