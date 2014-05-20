5b007bea96acf5b0d4171d65779a14b8666b956f
dhacker29 <dhackerdvm@gmail.com>
2014-05-20 05:58:32 -0400

    Gummify

---
+++ tg.mk
@@ -0,0 +1,43 @@
+# Copyright (C) 2014 TEAM Gummy
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file is the build configuration for a full Android
+# build for grouper hardware. This cleanly combines a set of
+# device-specific aspects (drivers) with a device-agnostic
+# product configuration (apps).
+#
+
+# Sample: This is where we'd set a backup provider if we had one
+# $(call inherit-product, device/sample/products/backup_overlay.mk)
+
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/oppo/find7a/full_find7a.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/Gummy/config/nfc_enhanced.mk)
+
+# Inherit some common Gummy stuff.
+$(call inherit-product, vendor/Gummy/config/common_full_phone.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 1920
+
+# Discard inherited values and use our own instead.
+PRODUCT_NAME := tg_find7a
+PRODUCT_DEVICE := find7a
+PRODUCT_BRAND := OPPO
+PRODUCT_MANUFACTURER := OPPO
+PRODUCT_MODEL := Find7a
