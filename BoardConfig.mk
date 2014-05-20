5344a944ba30c65367ec516633bfee60ce55269f
dhacker29 <dhackerdvm@gmail.com>
2014-05-20 15:19:46 -0400

    Set selinux permissive until we establish policy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -116,7 +116,7 @@ BOARD_CUSTOM_BOOTIMG_MK := device/oppo/find7a/mkbootimg.mk
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 androidboot.selinux=permissive
 BOARD_KERNEL_SEPARATED_DT := true
 
 # QCOM hardware
