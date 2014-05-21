d218dc04bdf7bb46a87d86c939edad55a17f6bc5
dhacker29 <dhackerdvm@gmail.com>
2014-05-21 04:47:08 -0400

    Add NFC permissions

--- device_find7a.mk
+++ device_find7a.mk
@@ -198,6 +198,12 @@ PRODUCT_PACKAGES += \
     Tag \
     com.android.nfc_extras
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
+
 # Power
 PRODUCT_PACKAGES += \
     power.msm8974
